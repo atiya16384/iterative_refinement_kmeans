ec20a99a2dc5ab0053ee754c9cf98fa72f19ec37
Mahboob <mahbooba@vdi-scclab-0017.lancs.local>
2025-06-12 00:17:46 +0100

    feat: set up k means

--- example.py
+++ example.py
@@ -6,8 +6,14 @@ from aoclda.sklearn import skpatch
 from sklearn.metrics import silhouette_score
 from sklearn.metrics import davies_bouldin_score
 
+
+
 def compare_precision():
-    X, y_true = make_blobs(n_samples=10000, centers=3, cluster_std=1.0, random_state=40)
+    dataset_sizes = [1000, 3000, 5000]
+    max_iter = 300
+    n_clusters = 4
+    n_features = 2
+    n_repeats = 3
 
     # Create dictionary of precisions
     precisions = {
@@ -15,46 +21,20 @@ def compare_precision():
         "Double Precision": np.float64
     }
     # Store results
-    results = {}
+    results = []
 
-    # Loop over precisions
-    for precision_name, dtype in precisions.items():
-        print(f"\nRunning KMeans with {precision_name}:")
+    for n_samples in dataset_sizes:
+        for repeat in range(n_repeats):
+            print(f"Dataset size{n_samples}, Repeat {repeat + 1}")
 
-        # Convert data to correct precision
-        X_precision = X.astype(dtype)
+            # Generate the data
+            X , y_true = make_blobs(n_samples=n_samples, n_features=n_features,centers= n_clusters, random_state=repeat )
 
-        # Apply KMeans
-        kmeans = KMeans(n_clusters=3, random_state=40)
-        kmeans.fit(X_precision)
+            # Common initilisation centers
 
-        # Get labels and centers
-        labels = kmeans.labels_
-        centers = kmeans.cluster_centers_
-
-        # Evaluate clustering accuracy
-        ari = adjusted_rand_score(y_true, labels)
-        silhouette_avg = silhouette_score(X_precision, labels)
-        db_index = davies_bouldin_score(X_precision, labels)
-
-        # Store results for comparison 
-        results[precision_name] = {
-            'labels': labels,
-            'centers': centers,
-            'ari': ari,
-            'silhoutte_avg': silhouette_avg,
-            'db_index': db_index
-        } 
-
-        print("Adjusted Rand Index:", ari)
-        print("Cluster Centers:\n", centers)
-        print("silhoutte_avg", silhouette_avg)
-        print("Db_index", db_index)
-        
-    # Absolute difference between centers 
-    center_diff = np.abs(results["Single Precision"]['centers'] -
-                        results["Double Precision"]['centers'])
-    print("\nAbsolute difference between cluster centers (float32 vs float64):\n", center_diff)
+            init_kmeans = KMeans(n_clusters=n_clusters, init = "k-means++", n_init = 1, random_state=repeat)
+            init_kmeans.fit(X.astype(np.float64))
+            initial_centers = init_kmeans.cluster_centers_
 
 skpatch()       
 compare_precision()
