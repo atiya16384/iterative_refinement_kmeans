e5e4f27afd7654b754c92ec44607f0e207a4c225
Mahboob <mahbooba@vdi-scclab-0017.lancs.local>
2025-06-12 00:33:59 +0100

    feat: set up comparison for single and double precision

--- example.py
+++ example.py
@@ -1,3 +1,4 @@
+from statistics import mode
 import numpy as np
 from sklearn.datasets import make_blobs
 from sklearn.cluster import KMeans
@@ -5,8 +6,7 @@ from sklearn.metrics import adjusted_rand_score
 from aoclda.sklearn import skpatch
 from sklearn.metrics import silhouette_score
 from sklearn.metrics import davies_bouldin_score
-
-
+import time
 
 def compare_precision():
     dataset_sizes = [1000, 3000, 5000]
@@ -35,6 +35,28 @@ def compare_precision():
             init_kmeans = KMeans(n_clusters=n_clusters, init = "k-means++", n_init = 1, random_state=repeat)
             init_kmeans.fit(X.astype(np.float64))
             initial_centers = init_kmeans.cluster_centers_
+
+
+            for precison_name, dtype in precisions.items():
+                X_precision = X.astype(dtype)
+                init_precision = initial_centers.astype(dtype)
+                
+                start_time = time.time()
+                kmeans = KMeans(n_clusters=n_clusters, init=init_precision, n_init=1, max_iter = max_iter, random_state=repeat)
+                kmeans.fit(X_precision)
+                elapsed = time.time() - start_time
+
+                labels = kmeans.labels_
+                centers = kmeans.cluster_centers_
+
+                ari = adjusted_rand_score(y_true, labels)
+                silhoutte_avg = silhouette_score(X_precision, labels)
+                db_index = davies_bouldin_score(X_precision, labels)
+                inertia = kmeans.inertia_
+                mem_MB = X_precision.nbytes/ 1e6
+
+                results.append([n_samples, max_iter, mode, 0, elapsed, mem_MB, ari, silhoutte_avg, db_index, inertia ])
+
 
 skpatch()       
 compare_precision()
