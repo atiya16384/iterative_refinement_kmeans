00ee146ba6c01605e583dce49bbac5509240f0b4
Mahboob <mahbooba@vdi-scclab-0022.lancs.local>
2025-07-01 20:10:54 +0100

    udpate command list

--- setup_env.sh
+++ setup_env.sh
@@ -5,3 +5,4 @@ source .venv/bin/activate
 pip install --upgrade pip
 python3 -m pip install numpy pandas matplotlib scikit-learn 
 pip install git+https://github.com/IntelPython/scikit-learn_bench.git
+python3 -m pip install scikit-learn-bench
