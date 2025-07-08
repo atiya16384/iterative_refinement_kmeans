d898f7824d5b2804579044cfffefa79e643f736d
Mahboob <mahbooba@vdi-scclab-0035.lancs.local>
2025-07-08 17:50:59 +0100

    save commands

--- setup_env.sh
+++ setup_env.sh
@@ -6,3 +6,4 @@ pip install --upgrade pip
 python3 -m pip install numpy pandas matplotlib scikit-learn 
 pip install git+https://github.com/IntelPython/scikit-learn_bench.git
 python3 -m pip install scikit-learn-bench
+python3 -m pip install ../aoclda-5.0.0-cp310-cp310-linux_x86_64.whl 
