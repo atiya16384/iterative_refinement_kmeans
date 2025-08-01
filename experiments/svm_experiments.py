e173eebc057d944d30abce78e71ce861a5c380b5
Atiya Mahboob <atiyahussain17170@outlook.com>
2025-08-01 22:24:55 +0100

    Update svm_experiments.py

--- experiments/svm_experiments.py
+++ experiments/svm_experiments.py
@@ -1,28 +1,31 @@
 from experiments.svm_precision import svm_double_precision, svm_hybrid_precision
 
-def run_all(tag, X, y, config, results_A, results_B, results_C, results_D):
-    n_repeats = config["n_repeats"]
-    tol_fixed_A = config["tol_fixed_A"]
-    tol_double_B = config["tol_double_B"]
-    caps = config["caps"]
-    tolerances = config["tolerances"]
-    max_iter_C = config["max_iter_C"]
-    perc_C = config["perc_C"]
-    tol_D = config["tol_D"]
+class SVMExperimentRunner:
+    def __init__(self, config):
+        self.config = config
+        self.results_A = []
+        self.results_B = []
+        self.results_C = []
+        self.results_D = []
 
-    for _ in range(n_repeats):
-        # A
-        for cap in caps:
-            results_A.append(svm_double_precision(tag, X, y, max_iter=300, tol=tol_fixed_A, cap=cap))
-            results_A.append(svm_hybrid_precision(tag, X, y, max_iter_total=300, tol_single=tol_fixed_A, tol_double=tol_fixed_A, single_iter_cap=cap))
-        # B
-        for tol in tolerances:
-            results_B.append(svm_double_precision(tag, X, y, max_iter=1000, tol=tol, cap=1000))
-            results_B.append(svm_hybrid_precision(tag, X, y, max_iter_total=1000, tol_single=tol, tol_double=tol_double_B, single_iter_cap=1000))
-        # C
-        cap_80 = int(max_iter_C * perc_C)
-        results_C.append(svm_double_precision(tag, X, y, max_iter=max_iter_C, tol=tol_fixed_A, cap=cap_80))
-        results_C.append(svm_hybrid_precision(tag, X, y, max_iter_total=max_iter_C, tol_single=tol_fixed_A, tol_double=tol_fixed_A, single_iter_cap=cap_80))
-        # D
-        results_D.append(svm_double_precision(tag, X, y, max_iter=1000, tol=tol_D, cap=1000))
-        results_D.append(svm_hybrid_precision(tag, X, y, max_iter_total=1000, tol_single=tol_D, tol_double=tol_double_B, single_iter_cap=1000))
+    def run_all(self, tag, X, y):
+        cfg = self.config
+        for _ in range(cfg["n_repeats"]):
+            # A: Vary cap
+            for cap in cfg["caps"]:
+                self.results_A.append(svm_double_precision(tag, X, y, max_iter=300, tol=cfg["tol_fixed_A"], cap=cap))
+                self.results_A.append(svm_hybrid_precision(tag, X, y, max_iter_total=300, tol_single=cfg["tol_fixed_A"], tol_double=cfg["tol_fixed_A"], single_iter_cap=cap))
+            # B: Vary tolerance
+            for tol in cfg["tolerances"]:
+                self.results_B.append(svm_double_precision(tag, X, y, max_iter=1000, tol=tol, cap=1000))
+                self.results_B.append(svm_hybrid_precision(tag, X, y, max_iter_total=1000, tol_single=tol, tol_double=cfg["tol_double_B"], single_iter_cap=1000))
+            # C: Fixed tol, 80% cap
+            cap_80 = int(cfg["max_iter_C"] * cfg["perc_C"])
+            self.results_C.append(svm_double_precision(tag, X, y, max_iter=cfg["max_iter_C"], tol=cfg["tol_fixed_A"], cap=cap_80))
+            self.results_C.append(svm_hybrid_precision(tag, X, y, max_iter_total=cfg["max_iter_C"], tol_single=cfg["tol_fixed_A"], tol_double=cfg["tol_fixed_A"], single_iter_cap=cap_80))
+            # D: Fixed tol
+            self.results_D.append(svm_double_precision(tag, X, y, max_iter=1000, tol=cfg["tol_D"], cap=1000))
+            self.results_D.append(svm_hybrid_precision(tag, X, y, max_iter_total=1000, tol_single=cfg["tol_D"], tol_double=cfg["tol_double_B"], single_iter_cap=1000))
+
+    def get_results(self):
+        return self.results_A, self.results_B, self.results_C, self.results_D
