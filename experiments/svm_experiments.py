deff5acd22b969319f7ab77e819ea0738c7ca830
Atiya Mahboob <atiyahussain17170@outlook.com>
2025-08-03 18:09:51 +0100

    Update svm_experiments.py

--- experiments/svm_experiments.py
+++ experiments/svm_experiments.py
@@ -19,13 +19,6 @@ class SVMExperimentRunner:
             for tol in cfg["tolerances"]:
                 self.results_B.append(svm_double_precision(tag, X, y, max_iter=1000, tol=tol, cap=1000))
                 self.results_B.append(svm_hybrid_precision(tag, X, y, max_iter_total=1000, tol_single=tol, tol_double=cfg["tol_double_B"], single_iter_cap=1000))
-            # C: Fixed tol, 80% cap
-            cap_80 = int(cfg["max_iter_C"] * cfg["perc_C"])
-            self.results_C.append(svm_double_precision(tag, X, y, max_iter=cfg["max_iter_C"], tol=cfg["tol_fixed_A"], cap=cap_80))
-            self.results_C.append(svm_hybrid_precision(tag, X, y, max_iter_total=cfg["max_iter_C"], tol_single=cfg["tol_fixed_A"], tol_double=cfg["tol_fixed_A"], single_iter_cap=cap_80))
-            # D: Fixed tol
-            self.results_D.append(svm_double_precision(tag, X, y, max_iter=1000, tol=cfg["tol_D"], cap=1000))
-            self.results_D.append(svm_hybrid_precision(tag, X, y, max_iter_total=1000, tol_single=cfg["tol_D"], tol_double=cfg["tol_double_B"], single_iter_cap=1000))
 
     def get_results(self):
-        return self.results_A, self.results_B, self.results_C, self.results_D
+        return self.results_A, self.results_B, 
