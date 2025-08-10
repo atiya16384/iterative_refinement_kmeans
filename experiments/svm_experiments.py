7366ccb95d5c9ebbbfe03607001f43a6f4d22f9e
Atiya Mahboob <atiyahussain17170@outlook.com>
2025-08-10 21:41:11 +0100

    Update svm_experiments.py

--- experiments/svm_experiments.py
+++ experiments/svm_experiments.py
@@ -16,7 +16,7 @@ class SVMExperimentRunner:
                 self.results_A.append(svm_hybrid_precision(tag, X, y, max_iter_total=300, tol_single=cfg["tol_fixed_A"], tol_double=cfg["tol_fixed_A"], single_iter_cap=cap))
             # B: Vary tolerance
             for tol in cfg["tolerances"]:
-                self.results_B.append(svm_double_precision(tag, X, y, max_iter=1000, tol=tol, cap=1000))
+                self.results_B.append(svm_double_precision(tag, X, y, max_iter=1000, tol=cfg["tol_double_B"], cap=1000))
                 self.results_B.append(svm_hybrid_precision(tag, X, y, max_iter_total=1000, tol_single=tol, tol_double=cfg["tol_double_B"], single_iter_cap=1000))
 
     def get_results(self):
