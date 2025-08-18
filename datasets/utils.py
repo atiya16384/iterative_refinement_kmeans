5fcee3d7bf3c5c28dcaac7794cdcd9a40507afa8
Atiya Mahboob <atiyahussain17170@outlook.com>
2025-08-18 21:11:12 +0100

    Update utils.py

--- datasets/utils.py
+++ datasets/utils.py
@@ -79,7 +79,15 @@ lr_columns_A = [  'DatasetName', 'DatasetSize', 'NumClasses', 'Mode', 'Cap', 'to
 
 lr_columns_B = [ 'DatasetName', 'DatasetSize', 'NumClasses','Mode', 'tolerance_single', 'iter_single', 'iter_double', 'Suite', 'Time', 'Memory_MB', 'Accuracy']
 
-
+# Column schemas (used by main to build DataFrames)
+en_columns_A = [
+    "DatasetName","NumFeatures","Mode","Cap","tolerance_single",
+    "iter_single","iter_double","Suite","Time","Memory_MB","R2","MSE"
+]
+en_columns_B = [
+    "DatasetName","NumFeatures","Mode","tolerance_single",
+    "iter_single","iter_double","Suite","Time","Memory_MB","R2","MSE"
+]
 
 
 
